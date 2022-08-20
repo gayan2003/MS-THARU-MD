b1d6fe01b551c337ad54f3f8e02feb96803978e4
gayan2003 <94740662+gayan2003@users.noreply.github.com>
2022-08-20 19:07:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,7 +40,7 @@ global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.available = false //auto available (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
 global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
